c25dc1b892cfa9b334c769beef61cc46f41381cf
Sam Corzine <SamCorzine@Sams-MacBook-Pro-5.local>
2018-01-29 10:40:11 -0600

    idk it compiles

--- hello/main.go
+++ hello/main.go
@@ -1,11 +1,45 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 type SandNode struct{
   x, y int
   value int
   n, s, e, w *SandNode
+}
+
+type SandPile struct{
+  root *SandNode
+}
+
+func addPile(p1 SandPile, p2 SandPile) SandPile {
+  var newroot = addNode(*(p1.root), *(p2.root))
+  return SandPile{root: &newroot}
+}
+
+
+func addNode(n1 SandNode, n2 SandNode) SandNode {
+  var newnorth = addNode(*n1.n, *n2.n)
+  var newsouth = addNode(*n1.s, *n2.s)
+  var neweast = addNode(*n1.e, *n2.e)
+  var newwest = addNode(*n1.w, *n2.w)
+  return SandNode{x: n1.x, y: n1.y, value: n1.value + n2.value, n: &newnorth,s: &newsouth,e: &neweast,w: &newwest }
+}
+
+func (n SandNode) isValid() bool {
+  if (n.value >= 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+func (n SandNode) isStable() bool{
+  if (n.value < 4) {
+    return true
+  } else {
+    return false
+  }
 }
 
 func (n SandNode) dirMap(s string) *SandNode{
@@ -49,4 +83,5 @@ func addnode(old SandNode, new SandNode, direction string){
 }
 
 func main(){
+
 }
